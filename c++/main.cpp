ac00cd1753bd4d8d3b7f7d453a0aee4554e83185
Joschua Gosda <joschua.gosda@control.lth.se>
2022-03-24 12:07:12 +0100

    forward kinematics for both arms

--- c++/main.cpp
+++ c++/main.cpp
@@ -14,25 +14,43 @@
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 
-
 	rl::mdl::UrdfFactory factory;
-	std::shared_ptr<rl::mdl::Model> model(factory.create("/home/joschua/Coding/invKinematics4/master-project/c++/src/urdf2/yumi_right.urdf"));
-	
-	rl::mdl::Kinematic* kinematic = dynamic_cast<rl::mdl::Kinematic*>(model.get());
-	rl::math::Vector q(7);
-	q << 10, 10, -20, 30, 50, -10 ,-10;
+	std::shared_ptr<rl::mdl::Model> model_r(factory.create("/home/joschua/Coding/invKinematics4/master-project/c++/src/urdf/yumi_right.urdf"));
+	std::shared_ptr<rl::mdl::Model> model_l(factory.create("/home/joschua/Coding/invKinematics4/master-project/c++/src/urdf/yumi_left.urdf"));
+	rl::mdl::Kinematic* kinematic_r = dynamic_cast<rl::mdl::Kinematic*>(model_r.get());
+	rl::mdl::Kinematic* kinematic_l = dynamic_cast<rl::mdl::Kinematic*>(model_l.get());
+	rl::math::Vector q_r(7);
+	rl::math::Vector q_l(7);
+	q_r << 0, 0, 30, 40, 50, 0, 0;
+	q_l << -10, 0, 10, 0, -20, 0, 0;
 	// 90 degrees at forth position is an offset
 	//q << -40, -90, 0, 90, 0, 0, 0;
-	q *= rl::math::DEG2RAD;
-	kinematic->setPosition(q);
-	kinematic->forwardPosition();
-	kinematic->calculateJacobian();
+	q_r *= rl::math::DEG2RAD;
+	q_l *= rl::math::DEG2RAD;
 
-	rl::math::Transform t = kinematic->getOperationalPosition(0);
-	rl::math::Vector3 position = t.translation();
-	rl::math::Vector3 orientation = t.rotation().eulerAngles(2, 1, 0).reverse();
-	std::cout << "Joint configuration in degrees: " << q.transpose() * rl::math::RAD2DEG << std::endl;
-	std::cout << "End-effector position: [m] " << position.transpose() << " orientation [deg] " << orientation.transpose() * rl::math::RAD2DEG << std::endl;
+	// forward kinematics for the right arm
+	kinematic_r->setPosition(q_r);
+	kinematic_r->forwardPosition();
+	kinematic_r->calculateJacobian();
+
+	// forward kinematics for the left arm
+	kinematic_l->setPosition(q_l);
+	kinematic_l->forwardPosition();
+	kinematic_l->calculateJacobian();
+
+	// extract orientation and position for the right arm
+	rl::math::Transform t_r = kinematic_r->getOperationalPosition(0);
+	rl::math::Vector3 position_r = t_r.translation();
+	rl::math::Vector3 orientation_r = t_r.rotation().eulerAngles(2, 1, 0).reverse();
+	std::cout << "Right Arm: Joint configuration in degrees: " << q_r.transpose() * rl::math::RAD2DEG << std::endl;
+	std::cout << "Right Arm: End-effector position: [m] " << position_r.transpose() << " orientation [deg] " << orientation_r.transpose() * rl::math::RAD2DEG << std::endl;
+	
+	// extract orientation and position for the left arm
+	rl::math::Transform t_l = kinematic_l->getOperationalPosition(0);
+	rl::math::Vector3 position_l = t_l.translation();
+	rl::math::Vector3 orientation_l = t_l.rotation().eulerAngles(2, 1, 0).reverse();
+	std::cout << "Left Arm: Joint configuration in degrees: " << q_l.transpose() * rl::math::RAD2DEG << std::endl;
+	std::cout << "Left Arm: End-effector position: [m] " << position_l.transpose() << " orientation [deg] " << orientation_l.transpose() * rl::math::RAD2DEG << std::endl;
 	
 	return 0;
 }
