8b2509872a0386118de50cd83177d8112f5e3e01
Joschua Gosda <joschua.gosda@control.lth.se>
2022-04-01 11:05:44 +0200

    implemented interpolation algorithm to genPaths.py

--- c++/main.cpp
+++ c++/main.cpp
@@ -16,7 +16,6 @@
 
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
 
 	rl::mdl::UrdfFactory factory;
 	std::shared_ptr<rl::mdl::Model> model_r(factory.create("/home/joschua/Coding/invKinematics4/master-project/c++/src/urdf/yumi_right.urdf"));
@@ -25,10 +24,8 @@ int main(int, char**) {
 	rl::mdl::Kinematic* kinematic_l = dynamic_cast<rl::mdl::Kinematic*>(model_l.get());
 	rl::math::Vector q_r(7);
 	rl::math::Vector q_l(7);
-	q_r << 0, 0, 0, 0, 0, 0, 0;
+	q_r << 10, 10, 10, 10, 10, 10, 10;
 	q_l << -10, 0, 10, 0, -20, 0, 0;
-	// 90 degrees at forth position is an offset
-	//q << -40, -90, 0, 90, 0, 0, 0;
 	q_r *= rl::math::DEG2RAD;
 	q_l *= rl::math::DEG2RAD;
 
@@ -50,6 +47,7 @@ int main(int, char**) {
 	// check if matrices are the same
 	std::cout << "RLJacobian \n" << kinematic_r->getJacobian() << std::endl;
 	std::cout << "myJacobian \n" << myJacobian_r << std::endl;
+	std::cout << "Manipulability meassure \n" << kinematic_r->calculateManipulabilityMeasure() << std::endl;
 	
 
 	// forward kinematics for the left arm
@@ -79,13 +77,14 @@ int main(int, char**) {
 	Eigen::Matrix<double, 7, 1> nullSpaceGradient;
 	Eigen::Matrix<double, 6, 1> actualPosition;
 	Eigen::Matrix<double, 6, 1> desPosition;
+	Eigen::Matrix<double, 6, 1> dPosition;
 	Eigen::Matrix<double, 7, 1> jointVelocity;
 	Eigen::Matrix<double, 6, 1> desVelocity;
 
 	// instantiate vars for cpg
 	Eigen::Matrix< double, 7, 1> q_min;
 	Eigen::Matrix< double, 7, 1> q_max;
-	Eigen::Matrix< double, 7, 1> q_pose;
+	//Eigen::Matrix< double, 7, 1> q_pose;
 
 	// does not to be modified?
 	weightingFactors << 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0;
@@ -95,38 +94,40 @@ int main(int, char**) {
 	nullSpaceGradient << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
 
 	// obtained from forward kinematics, later the current configuration read from egm interface
-	actualPosition << 0.564465, 0.00153031,   0.418386, 52.44*rl::math::DEG2RAD, -151.976*rl::math::DEG2RAD,  157.197*rl::math::DEG2RAD;
+	//actualPosition << 0.564465, 0.00153031,   0.418386, 52.44*rl::math::DEG2RAD, -151.976*rl::math::DEG2RAD,  157.197*rl::math::DEG2RAD;
+	actualPosition << position_r.transpose()(0), position_r.transpose()(1), position_r.transpose()(2), orientation_r.transpose()(0), orientation_r.transpose()(1), orientation_r.transpose()(2);
 	// add 1 mm to the position part
-	desPosition << 0.574465, 0.01153031,   0.428386, 52.44*rl::math::DEG2RAD, -151.976*rl::math::DEG2RAD,  157.197*rl::math::DEG2RAD;
+	dPosition << 0.01, 0.01, 0.01, 0.0, 0.0, 0.0;
+	desPosition = actualPosition + dPosition;
+	std::cout << "desPosition \n" << desPosition << std::endl;
 	// apply vel that results from actualPosition and desPosition
-	desVelocity << 0.2, 0.2, 0.2, 0.0, 0.0, 0.0;
+	desVelocity << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
 
 	jointVelocity << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
 	const double activationFactor = 1.0;
 	const double dt = 0.005;
 
-	/*
+	
 	// max and min joint values
 	q_min << -168.5, -143.5, -168.5, -123.5, -290, -88, -229;
 	q_min *= rl::math::DEG2RAD;
 	q_max << 168.5, 43.5, 168.5, 80, 290, 138, 229;
 	q_max *= rl::math::DEG2RAD;
 	// q_pose << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-	*/
+	
 
 
-	/*
+	
 	// create CompfortPoseGradient object
 	broccoli::control::ComfortPoseGradient<7> cpg;
 	cpg.useRangeBasedScaling(q_min, q_max);
 	//cpg.setPose(q_pose);
-	//nullSpaceGradient = cpg.process(q_r, jointVelocity);
-	std::cout << "gradient" << nullSpaceGradient << std::endl;
-	*/
+	nullSpaceGradient = cpg.process(q_r, jointVelocity);
+	std::cout << "gradient \n" << nullSpaceGradient << std::endl;
+	
 
 	// create ASC object and execute its functions for inverse kinematics
 	broccoli::control::AutomaticSupervisoryControl<6,7> ik;
-	//broccoli::control::Signal<double> sig;
 	ik.setWeighingMatrix(weightingFactors);
 	ik.setTaskSpaceConstraintFactor(activationFactor);
 	// set feedback gain for effective desired velocity
@@ -137,20 +138,20 @@ int main(int, char**) {
 	// compute the desired velocities in taskspace
 	ik.process(myJacobian_r, actualPosition, nullSpaceGradient, dt);
 
-	// obtain the computed velocity in task space - //sig = ik.outputVelocity();
-	std::cout << "output ik \n" << ik.outputVelocity() << std::endl;
-
+	// obtain the computed velocity in task space
+	//std::cout << "output ik \n" << ik.outputVelocity() << std::endl;
+	//std::cout << "test \n" << ik.outputVelocity().value() << std::endl;
 	
 	// perform integration over one timestep to obtain positions that can be send to robot
 	Eigen::Matrix<double, 7, 1> dq_r;
-	// following values a read from ik.outputVelocity
-	dq_r << -0.04, 0.08, 0.12, -0.16, 0.08, -0.24, 0.0;
+	dq_r << ik.outputVelocity().value();
+	//std::cout << "test2 \n" << dq_r << std::endl;
 	dq_r *= dt;
 
-	std::cout << "current qs \n" << q_r << std::endl;
-	std::cout << "next qs \n" << q_r+dq_r << std::endl;
+	std::cout << "current qs in RAD \n" << q_r* rl::math::RAD2DEG << std::endl;
+	std::cout << "next qs in RAD \n" << (q_r+dq_r)* rl::math::RAD2DEG << std::endl;
 
-	// copy of code above for checking for correctness
+	// copy of code above to check for correctness
 	// forward kinematics for the right arm
 	kinematic_r->setPosition(q_r+dq_r);
 	kinematic_r->forwardPosition();
--- python/preprocessing/genPaths.py
+++ python/preprocessing/genPaths.py
@@ -26,11 +26,120 @@ with open('preprocessing/gcode.txt') as f:
 
 # extract original x & y of both tcp's
 pos_split = np.split(pos, 2, axis=1)
-pos1, pos2 = pos_split[0], pos_split[1] 
+pos1, pos2 = pos_split[0]*0.001, pos_split[1]*0.001 # transform to SI-units - [m]
+
+# TODO: compute trajectory based on path of g-code
+# set a cutting speed, sample rate and compute necessary
+# position at every time step
+c_speed = 3000 * 0.001 * 1/60 # define cutting speed as 300 mm/min
+st = 0.005 # highest possible sample time - 250 Hz
+c_speed_min, c_speed_max = None, None
+
+p1x = list() # empty tuple holding lists for modified position coordinates - x, y
+p1y = list()
+p2x = list()
+p2y = list()
+v1x = list()
+v1y = list()
+
+for i in range(len(pos1[:,0])-1):
+    # append original points
+    
+    if not p1x: 
+        p1x = [pos1[i,0]]
+        p1y = [pos1[i,1]]
+        p2x = [pos2[i,0]]
+        p2y = [pos2[i,1]]
+    else:
+        p1x.append(pos1[i,0])
+        p1y.append(pos1[i,1])
+        p2x.append(pos2[i,0])
+        p2y.append(pos2[i,1])
+
+
+    dp1 = np.array([[pos1[i+1,0] - pos1[i, 0], pos1[i+1,1] - pos1[i, 1]]]) # compute vector between two samples of g-code
+    dp2 = np.array([[pos2[i+1,0] - pos2[i, 0], pos2[i+1,1] - pos2[i, 1]]])
+    dp1_len = norm(dp1, 2)
+    dp2_len = norm(dp2, 2) # compute length of this vector
+    nr_sect = int(round(dp1_len/(c_speed*st))) # additional interpolation points between original g-code points
+    if not v1x: 
+        if nr_sect == 0:
+            v1x = [0]
+            v1y = [0]
+        else:
+            v1x = [dp1[0,0] * 1/(nr_sect*st)] # interpolation distances are one more than interpolation points
+            v1y = [dp1[0,1] * 1/(nr_sect*st)] # compute x and y velecoties for tcp1
+    else:
+        if nr_sect == 0:
+            v1x.append(0)
+            v1y.append(0)
+        else:
+            v1x.append(dp1[0,0] * 1/((nr_sect)*st)) # interpolation distances are one more than interpolation points
+            v1y.append(dp1[0,1] * 1/((nr_sect)*st))
+
+  
+
+    # save the minimum cutting speed for tcp1
+    if not c_speed_min:
+        c_speed_min = norm(np.array([[v1x[-1]],[v1y[-1]]]),2)
+    else:
+        if (c_speed_min > norm(np.array([[v1x[-1]],[v1y[-1]]]),2)):
+            c_speed_min = norm(np.array([[v1x[-1]],[v1y[-1]]]),2)
+
+    # save the maximum cutting speed for tcp1
+    if not c_speed_max:
+        c_speed_max = norm(np.array([[v1x[-1]],[v1y[-1]]]),2)
+    else:
+        if (c_speed_max < norm(np.array([[v1x[-1]],[v1y[-1]]]),2)):
+            c_speed_max = norm(np.array([[v1x[-1]],[v1y[-1]]]),2)
+
+    # append everthing to lists
+    if nr_sect > 1:
+        for j in range(nr_sect):
+            # tcp1
+            p1x.append(pos1[i,0] + j * dp1[0,0] * 1/(nr_sect)) 
+            p1y.append(pos1[i,1] + j * dp1[0,1] * 1/(nr_sect))
+            v1x.append(v1x[-1])
+            v1y.append(v1y[-1])
+            #tcp2
+            p2x.append(pos2[i,0] + j * dp2[0,0] * 1/nr_sect) 
+            p2y.append(pos2[i,1] + j* dp2[0,1] * 1/nr_sect)
+    
+
+# TODO: plot and test for different cutting speeds and check functionality
+p1 = np.hstack((np.array(p1x).reshape(len(p1x),1), np.array(p1y).reshape(len(p1y),1)))
+p2 = np.hstack((np.array(p2x).reshape(len(p2x),1), np.array(p2y).reshape(len(p2y),1)))
+v1 = np.hstack((np.array(v1x).reshape(len(v1x),1), np.array(v1y).reshape(len(v1y),1)))
+
+# do plots for tcp1
+fig = plt.figure()
+plt.scatter(p1[:,0], p1[:,1])
+plt.scatter(pos1[:,0], pos1[:,1])
+plt.show()
+
+fig = plt.figure()
+v1_abs = np.array(list(map(lambda x: norm(x, 2), v1))).reshape(len(v1[:,0]),1) 
+plt.plot(np.linspace(0, len(v1[:,0])-1, len(v1[:,0])).reshape(len(v1[:,0]),1), v1_abs)
+plt.show()
+
+# make a 3D visualization
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(p1[:,0], p1[:,1], np.zeros(len(p1[:,0])))
+ax.scatter(pos1[:,0], pos1[:,1], np.zeros(len(pos1[:,0])))
+ax.plot(p1[:,0], p1[:,1], np.squeeze(v1_abs))
+plt.show()
+
+# do plot for tcp2
+fig = plt.figure()
+plt.scatter(p2[:,0], p2[:,1])
+plt.scatter(pos2[:,0], pos2[:,1])
+plt.show()
+
 # number of points in paths
 pNum = len(pos[:,0])
 # length of wire, defined in 4-axis setup 
-wLen = 800
+wLen = 0.8 
 z1 = np.zeros((pNum,1))
 z2 = np.ones((pNum,1)) * wLen
 # append z axis to position vectors
@@ -70,7 +179,6 @@ for i in range(pNum):
     # rotation matrix
     # self defined convention: rotate around y axis, then around x axis the get from  initial frame to wire frame
     # rotation from initial to first frame (z-axis aligned with wire)
-    #R01 = Rx @ Ry 
     R01 = Ry @ Rx
     # rotation from first frame to initial frame
     R10 = np.transpose(R01)
@@ -79,15 +187,9 @@ for i in range(pNum):
     # vector 1 in inital frame
     r0_01 = np.array([[x1], [y1], [0]])
     # vector in wires direction in first frame
-    #r12 = np.array([[0], [0], [wLen], [1]])
-    #T01 = np.hstack((R01, r01))
-    #T01 = np.vstack((T01, np.array([0, 0, 0, 1])))
-
     r1_12 = np.array([[0], [0], [wLen]])
     r0_12 = R01 @ r1_12
     r0_02 = r0_12 + r0_01
-
-
 
     # store values, cut last entry being 1 that was added for the transformation
     pos2_[i,:] = np.transpose(r0_02)
@@ -113,9 +215,8 @@ ax.plot(pos2_ref[:,0],pos2_ref[:,1],pos2_ref[:,2])
 ax.set_xlim(0,170)
 ax.set_ylim(0,100)
 ax.set_zlim(795,805)
-
-#plt.plot(pos1_[:,0],pos1_[:,2], marker='o')
-#plt.plot(pos2_[:,0],pos2_[:,2], marker='o')
-#plt.plot(pos2_ref[:,0],pos2_ref[:,2], marker='o')
 plt.show()
+
+# TODO: in the end you wanna save py data as:
+# p1x p1y p1z p1alpha p1beta p1gamma v1x v1y v1z p1dalpha p1dbeta p1dgamma + p2
 
