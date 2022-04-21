8d6691c2abe93785e4ea4d0d635d993514a2e2a4
Joschua Gosda <joschua.gosda@control.lth.se>
2022-04-21 17:11:45 +0200

    resolved undefined referrence to RL, but still pybin11 linking problems

--- c++/cmake/FindRL.cmake
+++
@@ -1,8 +0,0 @@
-
-find_path(RL_INCLUDE_DIRS 
-        PATHS "/opt/rl-0.7.0"
-        )
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(Eigen3 REQUIRED_VARS EIGEN3_INCLUDE_DIRS)
