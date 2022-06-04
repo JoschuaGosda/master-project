9bb31664df4e218fa94a401bee0546dab9e62835
Joschua Gosda <joschua.gosda@control.lth.se>
2022-06-04 17:19:11 +0200

    tested whole state machine's functionality successfully

--- python/abb_egm_pyclient/abb_egm_pyclient/feedback/control_final.py
+++ python/abb_egm_pyclient/abb_egm_pyclient/feedback/control_final.py
@@ -15,11 +15,6 @@ import serial.tools.list_ports
 from tqdm import tqdm
 from stateMachine import FoamCuttingMachine
 
-'''
-Before running this script make sure that the starting pose of the robot (either real one or in RS) match the
-starting pose of the computed joint angles. This script sends desired joint position to the robot. It takes to real
-joint positions at the robot into account
-'''
 
 foamCutting = FoamCuttingMachine()
 
@@ -37,7 +32,7 @@ p1, v1, p2, v2, phi_delta, dphi = get_trajectory()
 p1, v1, p2, v2, phi_delta, dphi = transform2yumi_workspace(p1, v1, p2, v2, phi_delta, dphi)
 
 # define staring postition in workspace for left arm - found by try and error in RS
-p1_start_des = np.array([0.3, 0.2, 0.2])
+p1_start_des = np.array([0.32, 0.3, 0.1]) # synced fine pose was np.array([0.3, 0.2, 0.2])
 p1, p2 = place_trajectory(p1_start_des, p1, p2)
 
 # setup for UDP connection
@@ -47,15 +42,17 @@ rate = 80
 
 # take the first desired taskSpaceInput for the right arm and simulate force sensor data 
 #translation_R_start = copy.copy(p2[0, :])
-translation_R_start = np.array([0.3, -0.2,  0.2]) # starting pose synched with robot
+syncTranslation_R = np.array([0.3, -0.2,  0.2]) # starting pose synched with robot
 #rotation_R_start = copy.copy(phi_delta[0, :])
-rotation_R_start = np.array([0, 0, 0])
-desPose_R_start = np.concatenate((translation_R_start, rotation_R_start), axis=0) 
+syncRotation_R = np.array([0, 0, 0])
+syncPose_R = np.concatenate((syncTranslation_R, syncRotation_R), axis=0) 
+desPose_R_start = np.concatenate((p2[0, :], phi_delta[0, :]), axis=0) 
 
 #translation_L_start = copy.copy(p1[0, :])
-translation_L_start = np.array([0.3, 0.2, 0.2]) # starting pose synched with robot
-rotation_L_start = np.array([0, 0, 0])
-desPose_L_start = np.concatenate((translation_L_start, rotation_L_start), axis=0)
+syncTranslation_L = np.array([0.3, 0.2, 0.2]) # starting pose synched with robot
+syncRotation_L = np.array([0, 0, 0])
+syncPose_L = np.concatenate((syncTranslation_L, syncRotation_L), axis=0)
+desPose_L_start = np.concatenate((p1[0, :], phi_delta[0, :]), axis=0)
 
 egm_client_R = EGMClient(port=UDP_PORT_RIGHT)
 egm_client_L = EGMClient(port=UDP_PORT_LEFT)
@@ -63,7 +60,7 @@ egm_client_L = EGMClient(port=UDP_PORT_LEFT)
 yumi_right = invKin.Yumi("/home/joschua/Coding/forceControl/master-project/c++/models/urdf/yumi_right.urdf")
 yumi_right.set_operationPoint(1.0)
 yumi_right.set_kp(0.05)
-yumi_right.set_hybridControl(True)
+yumi_right.set_hybridControl(False)
 yumi_right.set_transitionTime(3.0)
 
 yumi_left = invKin.Yumi("/home/joschua/Coding/forceControl/master-project/c++/models/urdf/yumi_left.urdf")
@@ -103,8 +100,22 @@ while not arduino.in_waiting:
 i = 0
 force = 0.0 # 0.5 [N] seems to be a good value for the control
 timestamp = time.time()
-cutting = False
 traj_samples = len(p1[:, 0]) 
+moveToStartVel = 0.005
+
+# delta is for both arms the same
+poseDelta = desPose_R_start - syncPose_R
+
+numIterations = round(np.fabs(poseDelta).max()/moveToStartVel * rate)
+toStartPose_R = np.zeros((numIterations, 6))
+toStartPose_L = np.zeros((numIterations, 6))
+
+for i in range(numIterations):
+    toStartPose_L[i, :] = syncPose_L + poseDelta/numIterations * i
+    toStartPose_R[i, :] = syncPose_R + poseDelta/numIterations * i
+
+toStartVel_R = (toStartPose_R[1, :] - toStartPose_R[0, :]) * rate
+toStartVel_L = (toStartPose_L[1, :] - toStartPose_L[0, :]) * rate
 
 # arrays to store the results for later plotting
 log_realPose_R = np.zeros((traj_samples, 6))
@@ -122,10 +133,9 @@ log_force = np.zeros((traj_samples, 1))
 print("\n Force control only to tension the wire...")
 
 foamCutting.start_syncronizing()
-foamCutting.start_tensioning()
 
+i = 0
 while True and arduino.isOpen():
-    #print('current StateMachine state: ', foamCutting.current_state)
     # check for new force data
     if arduino.in_waiting: # get the number of bytes in the input buffer
         packet = arduino.readline() # type: bytes  
@@ -136,8 +146,52 @@ while True and arduino.isOpen():
         if (time.time() - timestamp) >= (1.0/rate):
             timestamp = time.time()
 
+            # get the current joints angles for the right arm
+            jointAngles_R = get_realJointAngles(egm_client_R)
+            jointAngles_L = get_realJointAngles(egm_client_L)
 
-        
+            # compute the resulting jointVelocities
+            if i > 0:
+                jointVelocities_R = (jointAngles_R - jointAngles_R_old)/rate
+                jointVelocities_L = (jointAngles_L - jointAngles_L_old)/rate
+
+            yumi_right.set_jointValues(jointAngles_R, jointVelocities_R)
+            yumi_right.set_desPoseVel(toStartPose_R[i, :], toStartVel_R)
+            yumi_right.process()
+            pose_R = yumi_right.get_pose()
+            poseError_R = pose_R - syncPose_R
+
+            yumi_left.set_jointValues(jointAngles_L, jointVelocities_L)
+            yumi_left.set_desPoseVel(toStartPose_L[i, :], toStartVel_L)
+            yumi_left.process()
+            pose_L = yumi_left.get_pose()
+            poseError_L = pose_L - syncPose_L
+
+            ik_jointAngles_R = yumi_right.get_newJointValues() # computed joint values from IK
+            ik_jointAngles_L = yumi_left.get_newJointValues()
+
+            # transform to degrees as egm wants it
+            des_conf_R = np.degrees(ik_jointAngles_R)
+            des_conf_L = np.degrees(ik_jointAngles_L)
+
+            egm_client_R.send_planned_configuration(logic2abb(des_conf_R))
+            egm_client_L.send_planned_configuration(logic2abb(des_conf_L))
+
+            # save joint values to compute joint velocities in the next iteration
+            jointAngles_R_old = copy.copy(jointAngles_R)
+            jointAngles_L_old = copy.copy(jointAngles_L)
+
+            i = i+1
+            if i == numIterations:
+                print('end effectors are at starting poses, ready to mount wire')
+                i = 0 # reset counter
+                foamCutting.start_mounting()
+                
+    elif (foamCutting.is_mountWire):
+        if keyboard.is_pressed('enter'):
+            yumi_right.set_hybridControl(True)
+            foamCutting.start_tensioning()
+
 
     # force control only until wire is tensioned
     elif foamCutting.is_tensionWire:
@@ -180,15 +234,12 @@ while True and arduino.isOpen():
 
             i = i+1
             if (force > 0.65) and keyboard.is_pressed('enter'):
-                cutting = True
-                jointVelocities_L = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-                jointVelocities_R = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
                 i = 0 # reset counter
                 print("Changing to hybrid control now...")
                 foamCutting.start_cutting()
                 pbar = tqdm(total=traj_samples)
     
-    # hybrid control - cutting is True
+    # hybrid control 
     elif foamCutting.is_cutting:
         if (time.time() - timestamp) >= (1.0/rate):
             timestamp = time.time()
@@ -250,9 +301,7 @@ while True and arduino.isOpen():
             jointAngles_R_old = copy.copy(jointAngles_R)
             i = i + 1 
             pbar.update(1)
-            #print("freq for one loop: ", 1/(time.time()- timestamp))
             
-
             if (i >= (traj_samples-1)):
                 pbar.close()
                 foamCutting.end_cutting()
---
+++ python/abb_egm_pyclient/abb_egm_pyclient/feedback/stateMachine.py
@@ -0,0 +1,46 @@
+from statemachine import StateMachine, State
+
+class FoamCuttingMachine(StateMachine):
+    # green = State('Green', initial=True)
+    # yellow = State('Yellow')
+    # red = State('Red')
+
+    # define states
+    standby = State('Standby', initial=True)
+    moveToStartPose = State('MoveToStartPose')
+    mountWire = State('MountWire')
+    tensionWire = State('TensionWire')
+    cutting = State('Cutting')
+
+    # define transitions
+    start_syncronizing = standby.to(moveToStartPose)
+    start_mounting = moveToStartPose.to(mountWire)
+    start_tensioning = mountWire.to(tensionWire)
+    start_cutting = tensionWire.to(cutting)
+    end_cutting = cutting.to(standby)
+
+    def on_enter_moveToStartPose(self):
+        print('State changed to moveToStartPose')
+
+    def on_enter_mountWire(self):
+        print('State changed to mountWire')
+
+    def on_enter_tensionWire(self):
+        print('State changed to tensionWire')
+
+    def on_enter_cutting(self):
+        print('State changed to cutting')
+
+    def on_enter_standby(self):
+        print('State changed to standby')
+
+
+    # slowdown = green.to(yellow)
+    # stop = yellow.to(red)
+    # go = red.to(green)
+
+    # states
+    # standby
+    # moveToStartPose
+    # tensionWire
+    # cutting
